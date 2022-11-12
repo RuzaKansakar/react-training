862e54a3a4427170f123d8567e9d476790012b7c
RuzaKansakar <ruzakansakar@gmail.com>
2022-11-12 08:30:41 +0545

    Built Tic Tac Toe

---
+++ src/components/Square.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+export default function Square(props)
+{
+    //const [value,setValue]=useState(null);
+    function handleClick(){
+        
+    }
+    return(
+        <button className="square" onClick={props.onClick}>
+                {props.value}
+       </button>
+    )
+}
