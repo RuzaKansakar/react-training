0fc8444d485824214d530d11aea6ab9ccba3a7f5
RuzaKansakar <ruzakansakar@gmail.com>
2022-11-13 19:27:42 +0545

    Made weather app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 
+import { useEffect,useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const [notifications, setNotifications]= useState(0) ;
+  useEffect(() =>
+  {
+    if (notifications>0){
+      document.title="You have" + notifications + "notifications"
+    }
+    
+  })
+  function sendNotifications(){
+    setNotifications(notifications+1)
+  }
   return (
     <div className="App">
-      
+      <button onClick={sendNotifications}>Send Notifications</button>
     </div>
   );
 }
