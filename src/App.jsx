961173a458299f9e629e64f7275bbfe4a2bbb93c
RuzaKansakar <ruzakansakar@gmail.com>
2022-11-26 09:39:04 +0545

    used react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 
 import './App.css';
-import Game from "./components/Game";
+import Game from"./components/Game";
+import {Link,Outlet,Route, Routes} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Game/>
+    <div>
+      <nav>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/game">Game</Link></li>
+        </ul>
+        
+        
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>} >
+          <Route path="/posts"  element={<Post />}/>
+          <Route path="/game" element={<Game />} />
+        </Route>
+        <Route path="/game" element={<Game />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Home()
+{
+  return (
+    <>
+    <div> Header</div>
+    <div>Sidebar</div>
+    <Outlet />
+    <div>Footer</div>
+    </>
+  )
+}
+
+function Post(){
+  return (
+    <div>
+    My posts
+    </div>
+  )
+}
+
